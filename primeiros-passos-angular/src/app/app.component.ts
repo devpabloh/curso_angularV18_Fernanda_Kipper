49e6cc3ab7aa0013dce9309b1f086c1cbf353048
Dev. Pablo H. <devpabloh@gmail.com>
2024-09-29 19:30:56 -0300

    criando o componente footer e o componente home e vendo como o standalone funciona

--- primeiros-passos-angular/src/app/app.component.ts
+++ primeiros-passos-angular/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root', // o seletor é aonde iremos colocar o nome que vamos chamar o component se precisar usar o mesmo em outro local
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, //comportamento dos modulos do angular, dentro do mesmo nós temos duas opções:
+  //standalone:
+  //NgModule:
+  imports: [RouterOutlet, HomeComponent],
   // template quando queremos passar toda a estrutura no próprio arquivo ts
   //template: "<h1>Olá</h1>",
 
